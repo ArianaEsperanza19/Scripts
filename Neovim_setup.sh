94e21ab0394350df757099befa03a3d331d68cb7
ArianaEsperanza19 <ArianaEsperanza19>
2025-12-03 00:40:54 -0400

    Mejorar script de Neovim para que solo se ejecute cuando nvim este instalado previamente

--- Neovim_setup.sh
+++ Neovim_setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 # Script para configurar Neovim/LazyVim mediante un repositorio GitHub.
 
-# Descargar devicons
-# Reemplaza estos valores con la URL de tu repositorio y el directorio de destino
-REPO_URL="https://github.com/ArianaEsperanza19/Lazyvim.git"
-DESTINO="$HOME/.config/nvim"
+if command -v nvim &>/dev/null; then
+	echo "✅ Neovim instalado. Versión: $(nvim --version | head -n 1)"
+	# Reemplaza estos valores con la URL de tu repositorio y el directorio de destino
+	REPO_URL="https://github.com/ArianaEsperanza19/Lazyvim.git"
+	DESTINO="$HOME/.config/nvim"
 
-# 1. Verificar si el directorio de destino existe, si no, lo crea.
-if [ ! -d "$DESTINO" ]; then
-	echo "El directorio de destino '$DESTINO' no existe. Creándolo..."
-	sudo mkdir -p "$DESTINO"
+	# 1. Verificar si el directorio de destino existe, si no, lo crea.
+	if [ ! -d "$DESTINO" ]; then
+		echo "El directorio de destino '$DESTINO' no existe. Creándolo..."
+		sudo mkdir -p "$DESTINO"
+	fi
+
+	# 2. Clonar el repositorio en una carpeta temporal
+	echo "Clonando el repositorio en una carpeta temporal..."
+	sudo git clone --depth 1 "$REPO_URL" "$DESTINO"
+
+	# Verificar si la clonación fue exitosa
+	if [ $? -ne 0 ]; then
+		echo "Error: No se pudo clonar el repositorio."
+		exit 1
+	fi
+
+	sudo chown -R ariana $DESTINO
+
+	echo "✅¡Hecho! Contenido del repositorio clonado exitosamente."
+else
+	echo "❌ Neovim no se encontró."
 fi
-
-# 2. Clonar el repositorio en una carpeta temporal
-echo "Clonando el repositorio en una carpeta temporal..."
-sudo git clone --depth 1 "$REPO_URL" "$DESTINO"
-
-# Verificar si la clonación fue exitosa
-if [ $? -ne 0 ]; then
-	echo "Error: No se pudo clonar el repositorio."
-	exit 1
-fi
-
-sudo chown -R ariana $DESTINO
-
-echo "✅¡Hecho! Contenido del repositorio clonado exitosamente."
